578c84e77d5cebf589aaef8e696a2864456affc6
Jinseong <aimer120@nate.com>
2021-08-30 17:39:20 +0900

    refactor: Add debounce function for delaying user name input

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -6,3 +6,12 @@ export const createBaseURL = (type) => {
 };
 
 export const getFixedDigits = (num) => (num < 10 ? `0${num}` : `${num}`);
+
+export const debounce = (func, delay) => {
+  let timeoutId = null;
+
+  return (e) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(func.bind(this, e), delay);
+  };
+};
